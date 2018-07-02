7580be1795ffc2cc7a54d20c76531efa42499875
astikute <agnese.stikute@gmail.com>
2018-07-03 02:41:53 +0300

    Reservation

--- ReservationSystem/src/CommunicateDb.java
+++ ReservationSystem/src/CommunicateDb.java
@@ -7,49 +7,42 @@ import java.sql.Time;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-import javax.swing.JOptionPane;
-import javax.swing.JRadioButton;
 import javax.swing.table.DefaultTableModel;
 
 public class CommunicateDb {
-	
-	public static Time convertIntoTime (String str) {
-	    DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-	    Date date = null;
+		
+	public static void displayReservations (Connection conn, String date, DefaultTableModel model) {
 		try {
-			date = dateFormat.parse(str);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-	    Time time = new Time(date.getTime());
-		return time;
-	}
-	
-	public static void displayReservations (Connection conn, DefaultTableModel model) {
-		try {
-			String queryString = "SELECT employees.id, name, last_name, rent_from, rent_till "
+			String queryString = "SELECT employees.id, name, last_name, rent_from, rent_till, rent_bike.bike_id "
 					+ "FROM employees "
 					+ "LEFT JOIN rent_bike ON employees.id=rent_bike.id "
+					+ "AND date = ? "
 					+ "ORDER BY employees.id";
 			PreparedStatement ps = conn.prepareStatement(queryString);
-			ResultSet results = ps.executeQuery(queryString);
+			ps.setString(1, date);
+			ResultSet results = ps.executeQuery();
 			while (results.next()) {
 				String dbId = results.getString("id");
 	    		String dbName = results.getString("name");
 	    		String dbLast = results.getString("last_name");
 	    		String dbFrom = results.getString("rent_from");
 	    		String dbTill = results.getString("rent_till");
+	    		String dbBike = results.getString("bike_id");
 	    		if (dbFrom != null && dbTill != null) {
 	    			dbFrom = dbFrom.substring(0, 5);
 	    			dbTill = dbTill.substring(0, 5);
 	    		}
-	    		model.addRow(new Object[] {dbId, dbName, dbLast, dbFrom, dbTill});
+	    		model.addRow(new Object[] {dbId, dbName, dbLast, dbFrom, dbTill, dbBike});
 			}
-			} catch (SQLException sql) {
-				System.out.println(sql);
-			}
+		} catch (SQLException sql) {
+			System.out.println(sql);
+		}
 	}
 	
 	public static boolean validateUser (Connection conn, String input) {
@@ -69,42 +62,72 @@ public class CommunicateDb {
 		return false;	
 	}
 	
-	public static boolean checkReservations (Connection conn, String inputTime1, String inputTime2) {
-		try {
-			String queryString = "SELECT rent_from, rent_till FROM rent_bike";
+	public static String checkReservations (Connection conn, String inputTime1, String inputTime2, String date) {
+		String[] arr = new String[100];
+		int i = 0;
+		Time time1 = convertIntoTime(inputTime1);
+		Time time2 = convertIntoTime(inputTime2);
+		Boolean rentAvailable;
+		
+		try { 
+			String queryString = "SELECT bike_id FROM electric_bikes";
 			PreparedStatement ps = conn.prepareStatement(queryString);
 			ResultSet results = ps.executeQuery(queryString);
-			Date time1 = convertIntoTime(inputTime1);
-			Date time2 = convertIntoTime(inputTime2);
-
-        	while (results.next()) {
-        			
-        		String dbFrom = results.getString("rent_from");
-        		Date db1 = convertIntoTime(dbFrom);
-        		String dbTill = results.getString("rent_till");
-        		Date db2 = convertIntoTime(dbTill);
-
-        		if ((db1.before(time1) || db1.compareTo(time1) == 0) && db2.after(time1)) {
-        			return false;
-        		}
-        		if (db1.before(time2) && (db2.after(time2) || db2.compareTo(time2) == 0)) {
-        			return false;
-        		}
-        	}
+			
+			while (results.next()) {
+				arr[i] = results.getString("bike_id");
+				i++;
+			}
 		} catch (SQLException sql) {
 			System.out.println(sql);
 		}
-		return true;
+		
+		for (int j = 0; j < arr.length; j++) {
+			rentAvailable = true;
+			try {
+				String queryString = "SELECT rent_from, rent_till "
+						+ "FROM rent_bike WHERE bike_id = ? and date = ?";
+				PreparedStatement ps = conn.prepareStatement(queryString);
+				ps.setString(1, arr[j]);
+				ps.setString(2, date);
+				ResultSet results = ps.executeQuery();
+			
+				while (results.next()) {
+					String dbFrom = results.getString("rent_from");
+					String dbTill = results.getString("rent_till");
+					Time db1 = convertIntoTime(dbFrom);
+					Time db2 = convertIntoTime(dbTill);
+					
+					if ((db1.before(time1) || db1.compareTo(time1) == 0) && db2.after(time1)) {
+						rentAvailable = false;
+						break;
+					}
+					if (db1.before(time2) && (db2.after(time2) || db2.compareTo(time2) == 0)) {
+						rentAvailable = false;
+						break;
+					}
+				}
+			} catch (SQLException sql) {
+				System.out.println(sql);
+			}
+			if (rentAvailable) {
+				return arr[j];
+			} else continue;
+		}
+		return null;
 	}
 	
-	public static void setReservations (Connection conn, String inputTime1, String inputTime2, String input) {
+	public static void setReservations (Connection conn, String inputTime1, String inputTime2, String input, String bikeId, String date) {
 		try {
-			String queryString = "INSERT INTO rent_bike (rent_from, rent_till, id) VALUES (?, ?, ?)";
+			String queryString = "INSERT INTO rent_bike (rent_from, rent_till, id, bike_id, date) "
+					+ "VALUES (?, ?, ?, ?, ?)";
 			PreparedStatement ps = conn.prepareStatement(queryString);
-			ps.setTime(1, convertIntoTime(inputTime1));
-			ps.setTime(2, convertIntoTime(inputTime2));
+			ps.setTime(1, modifyTime(inputTime1));
+			ps.setTime(2, modifyTime(inputTime2));
 			int parseInput = Integer.parseInt(input);
 			ps.setInt(3, parseInput);
+			ps.setString(4, bikeId);
+			ps.setString(5, date);
 			ps.execute();
 		} catch (SQLIntegrityConstraintViolationException e) {
 			MainDisplay.showMsg("Employee has already a reservation!");
@@ -112,14 +135,78 @@ public class CommunicateDb {
 		}
 	}
 	
-	public static void deleteReservations (Connection conn, int id) {
+	public static void deleteReservations (Connection conn, int id, String date) {
 		try {
-			String queryString = "DELETE FROM rent_bike WHERE id = ?";
+			String queryString = "DELETE FROM rent_bike WHERE id = ? and date = ?";
 			PreparedStatement ps = conn.prepareStatement(queryString);
 			ps.setInt(1, id);
+			ps.setString(2, date);
 			ps.execute();
 		} catch (SQLException sql) {
 			System.out.println(sql);
 		}
+	}
+	
+	public static String[] updateDate () {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar calendar = GregorianCalendar.getInstance();
+		String today = dateFormat.format(calendar.getTime());
+		calendar.add(GregorianCalendar.DAY_OF_MONTH, 1);
+		String tomorrow = dateFormat.format(calendar.getTime());
+		calendar = GregorianCalendar.getInstance();
+		calendar.add(GregorianCalendar.DAY_OF_MONTH, -1);
+		String yesterday = dateFormat.format(calendar.getTime());
+		String[] dates = {today, tomorrow, yesterday};
+		return dates;
+	}
+	
+	public static void deleteYesterdayReservations (Connection conn, String str) {
+		try {
+			String queryString = "DELETE FROM rent_bike WHERE date = ?";
+			PreparedStatement ps = conn.prepareStatement(queryString);
+			ps.setString(1, str);
+			ps.execute();
+		} catch (SQLException sql) {
+			System.out.println(sql);
+		}
+	}
+	
+	public static Time modifyTime (String str) {
+	    DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		try {
+			Date date = dateFormat.parse(str);
+		    Calendar calendar = GregorianCalendar.getInstance();
+		    calendar.setTime(date);
+		    calendar.add(GregorianCalendar.HOUR, -1);
+		    date = calendar.getTime();
+			Time time = new Time(date.getTime());
+			return time;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static Time convertIntoTime (String str) {
+	    DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+	    Date date = null;
+		try {
+			date = dateFormat.parse(str);
+			Time time = new Time(date.getTime());
+			return time;
+		} catch (ParseException e) {
+		}
+		return null;
+	}
+	
+	public static boolean isValidInput (String str) {
+		try {
+			new SimpleDateFormat("HH:mm").parse(str);
+			return true;
+		} catch (ParseException e) {
+			MainDisplay.showMsg("Invalid time!");
+			return false;
+		}
+		
 	}
 }
--- ReservationSystem/src/MainDisplay.java
+++ ReservationSystem/src/MainDisplay.java
@@ -2,8 +2,20 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.sql.Connection;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -14,13 +26,14 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
-public class MainDisplay extends JFrame implements ActionListener, ListSelectionListener  {
+public class MainDisplay extends JFrame implements ActionListener, ListSelectionListener, ItemListener  {
 	
 	Connection conn;
 	
 	JPanel tableField = new JPanel ();
 	static JPanel main = new JPanel ();
 	JTable table = new JTable();
+	JLabel dateLabel = new JLabel("Select date:");
 	JButton enterBtn = new JButton("Add");
 	JLabel userLabel = new JLabel("Employee Id:");
 	JTextField inputUser = new JTextField ();
@@ -30,21 +43,29 @@ public class MainDisplay extends JFrame implements ActionListener, ListSelection
 	JTextField inputTill = new JTextField ("00:00");
 	JButton deleteBtn = new JButton("Cancel reservation");
 	
-	Object [] columnNames = {"Id", "Name", "Last Name", "From", "Till"};
+	Object [] columnNames = {"Id", "Name", "Last Name", "From", "Till", "Bike Id"};
 	DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+	
+	String[] dates = CommunicateDb.updateDate();
+	JComboBox<String> dateList = new JComboBox<>(dates);
 	
 	public MainDisplay(Connection conn) {
 		
 		this.conn = conn;
 		
 		model.addRow(columnNames);
-		CommunicateDb.displayReservations(conn, model);
+		CommunicateDb.deleteYesterdayReservations(conn, dates[2]);
+		CommunicateDb.displayReservations(conn, dates[0], model);
 		table.setModel(model);
 		table.setRowHeight(table.getRowHeight()+5);
 		table.getSelectionModel().addListSelectionListener(this);
 		tableField.add(table);
 		add(tableField, BorderLayout.CENTER);
-		
+	
+		main.add(dateLabel);
+		dateList.removeItemAt(2);
+		dateList.addItemListener(this);
+		main.add(dateList);
 		main.add(userLabel);
 		main.add(inputUser);
 		inputUser.setColumns(4);
@@ -72,40 +93,52 @@ public class MainDisplay extends JFrame implements ActionListener, ListSelection
 		String input = inputUser.getText().trim();
 		String timeFrom = inputFrom.getText().trim();
 		String timeTill = inputTill.getText().trim();
-		Boolean isValidUser = false;
-		Boolean rentAvailable = false;
 		
-		if (input.length() != 0) { 
+		Boolean isValidInput1  = CommunicateDb.isValidInput(timeFrom);
+		Boolean isValidInput2  = CommunicateDb.isValidInput(timeTill);
+		Boolean isValidUser = false;
+		String bikeId = "";
+		
+		if (input.length() != 0) {
+		if (isValidInput1 && isValidInput2) { 
 			isValidUser = CommunicateDb.validateUser(conn, input);
 			if (isValidUser) {
-				rentAvailable = CommunicateDb.checkReservations(conn, timeFrom, timeTill);
-				if (rentAvailable) {
-					CommunicateDb.setReservations(conn, timeFrom, timeTill, input);
+				bikeId = CommunicateDb.checkReservations(conn, timeFrom, timeTill, dates[dateList.getSelectedIndex()]);
+				if (bikeId != null) {
+					CommunicateDb.setReservations(conn, timeFrom, timeTill, input, bikeId, dates[dateList.getSelectedIndex()]);
 					model.setRowCount(1);
-					CommunicateDb.displayReservations(conn, model);
-				} else JOptionPane.showMessageDialog(main, "Reservation is not possible!", "Message", JOptionPane.ERROR_MESSAGE);
+					CommunicateDb.displayReservations(conn, dates[dateList.getSelectedIndex()], model);
+				} else JOptionPane.showMessageDialog(main, "Reservation is not possible! No available bikes at chosen time.", "Message", JOptionPane.ERROR_MESSAGE);
 			} else JOptionPane.showMessageDialog(main, "Invalid id!", "Message", JOptionPane.ERROR_MESSAGE);
-		} else JOptionPane.showMessageDialog(main, "You forgot employee id.", "Message", JOptionPane.QUESTION_MESSAGE); 
+		}
+		} else JOptionPane.showMessageDialog(main, "You forgot employees id", "Message", JOptionPane.QUESTION_MESSAGE);
 		}
 		
 		if (e.getSource() == deleteBtn) {
 			int selectedRow = table.getSelectedRow();
 			int id = Integer.parseUnsignedInt((String) model.getValueAt(selectedRow, 0));
-			CommunicateDb.deleteReservations(conn, id);
+			CommunicateDb.deleteReservations(conn, id, dates[dateList.getSelectedIndex()]);
 			model.setRowCount(1);
-			CommunicateDb.displayReservations(conn, model);
+			CommunicateDb.displayReservations(conn, dates[dateList.getSelectedIndex()], model);
 			deleteBtn.setEnabled(false);
 		}
 	}
+	
+	public void itemStateChanged(ItemEvent ie) {
+		if (ie.getStateChange() == ItemEvent.SELECTED) {
+		int selectedDate = dateList.getSelectedIndex();
+		model.setRowCount(1);
+		CommunicateDb.displayReservations(conn, dates[selectedDate], model);
+		}
+    }
 
 	public void valueChanged(ListSelectionEvent arg0) {
-		
 		int selectedRow = table.getSelectedRow();
 		if (selectedRow > 0) {
 			if (model.getValueAt(selectedRow, 4) != null && model.getValueAt(selectedRow, 3) != null) {
 				deleteBtn.setEnabled(true); 
-			} else deleteBtn.setEnabled(false);
-		} 
+			} else deleteBtn.setEnabled(false); 
+		} else deleteBtn.setEnabled(false); 
 	}
 	
 	public static void showMsg (String str) {
